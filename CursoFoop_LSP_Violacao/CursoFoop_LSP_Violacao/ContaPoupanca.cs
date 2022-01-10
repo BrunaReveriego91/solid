d4a373425c708d58051e373262d598356290d47a
Bruna Reveriego <Bruna.Reveriego@br.ey.com>
2022-01-10 15:05:02 -0300

    exemplo de violação do principio de substituição de liskov

---
+++ CursoFoop_LSP_Violacao/CursoFoop_LSP_Violacao/ContaPoupanca.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoFoop_LSP_Violacao
+{
+    class ContaPoupanca : Conta
+    {
+        public override void Saque(double valor)
+        {
+            if (Saldo > valor)
+            {
+                Saldo -= valor;
+            }
+        }
+    }
+}
---
+++ CursoFoop_LSP_Violacao/CursoFoop_LSP_Violacao/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CursoFoop_LSP_Violacao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //usando a classe base
+            Console.WriteLine("LSP - Usando a classe base");
+            var conta = new Conta();
+            conta.Saldo = 100;
+            conta.Saque(250);
+            Console.WriteLine($"Saldo da conta: {conta.Saldo}");
+
+            var conta1 = new ContaPoupanca();
+            conta1.Saldo = 100;
+            conta1.Saque(250);
+            Console.WriteLine($"Saldo da conta poupança : {conta1.Saldo}");
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
